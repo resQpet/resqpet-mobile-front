6596638299aeda0d7d4e546ca2e2d9f5818ec20f
AjBonifacio <angelojose383@gmail.com>
2025-07-31 23:59:38 -0400

    feat: add Follows model and update follower service logic

---
+++ services/fundaciones/foundations/FollowerService.tsx
@@ -0,0 +1,32 @@
+import { BaseService } from "~/services/BaseService";
+import { OnFollowFoundation,FoundationFollowsCount } from "~/domain/models/foundations/FollowerFoundation";
+
+
+export class FollowerServices extends BaseService {
+
+    private static Factory = new FollowerServices();
+
+    static get instance() {
+        return FollowerServices.Factory;
+    }
+
+    constructor(){
+        super('/foundations')
+    }
+
+    async getOnfollow (IdFoandation: number) : Promise<OnFollowFoundation>{
+        return super.get<OnFollowFoundation>(`${IdFoandation}/follows`);
+    }
+
+    async getCountFollow (IdFoandation: number) : Promise<FoundationFollowsCount>{
+        return super.get<FoundationFollowsCount>(`${IdFoandation}/follows/count`);
+    }
+
+    async PostFollow (IdFoandation: number): Promise<void>{
+        return super.post(`${IdFoandation}/follows`)
+    }
+    
+    async DeleteFollow (IdFoandation:number): Promise<void>{
+        return super.put(`${IdFoandation}/follows`)
+    }
+}
